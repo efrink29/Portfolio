be5844c3d95bc403e9e270ecc0e7df908cafcc63
Ethan Frink <efrink@asu.edu>
2021-11-16 19:50:16 -0700

    git

---
+++ projects.js
@@ -0,0 +1,50 @@
+const GITHUB_USERNAME = "efrink29"
+fetch('https://api.github.com/users/efrink29/repos')
+.then(response => response.json())
+.then(repos => {
+    
+    for(const repo of repos) {
+
+        const repoName = repo.name
+        const repoDescription = repo.description
+        const repoURL = repo.html_url
+        const repoLanguage = repo.language
+
+        const cardContainer = document.createElement('div')
+        cardContainer.className = "card center"
+
+        const cardImage = document.createElement('img')
+        cardImage.src = "./assets/Pictures/generic_code_bgrnd.jpg"
+        cardImage.className = "project-image"
+
+        cardContainer.appendChild(cardImage)
+
+        const projectInfoContainer = document.createElement('div')
+        projectInfoContainer.className = "project-info-container"
+
+        const projectTitle = document.createElement('h3')
+        projectTitle.textcContent = repoName
+
+        const projectDescription = document.createElement('p')
+        projectDescription.textContent = repoDescription
+
+        const projectURL = document.createElement('a')
+        projectURL.href = repoURL
+        projectURL.target = "_blank"
+        projectURL.appendChild(projectTitle)
+
+        const projectLanguage = document.createElement('p')
+        projectLanguage.textContent = repoLanguage
+
+        projectInfoContainer.appendChild(projectURL)
+        projectInfoContainer.appendChild(projectDescription)
+        projectInfoContainer.appendChild(projectTitle)
+
+        cardContainer.appendChild(projectInfoContainer)
+        
+        const projectsContainer = document.getElementById('projects_container')
+
+        projectsContainer.appendChild(cardContainer)
+
+    }
+})
